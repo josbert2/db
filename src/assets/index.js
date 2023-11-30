87ab03a631f5bcbaaf2058ce284bd9edb2d527f9
unknown <joheandroid@gmail.com>
2023-11-30 18:07:32 -0300

    fix

---
+++ src/assets/index.js
@@ -0,0 +1,42 @@
+export { default as header } from './header.png';
+export { default as abejaazul } from './abejaazul.png';
+export { default as ulmonivel } from './ulmonivel.png';
+export { default as hulmoorcanico } from './hulmoorcanico.png';
+export { default as ulmo1 } from './1.png';
+
+
+export { default as hiervaazul } from './hiervaazul.png';
+export { default as hiervaazulabejita } from './hiervaazulabejita.png';
+export { default as pex } from './pex.webp';
+
+export { default as somosheader } from './somosheader.png';
+
+export { default as header2 } from './header2.png';
+export { default as header3 } from './header3.png';
+export { default as header1 } from './header1.png';
+
+
+export { default as finca } from './finca.png';
+export { default as siempre } from './siempre.jpg';
+export { default as innvo } from './innvo.jpg';
+export { default as aprendizaje } from './aprendizaje.jpg';
+
+
+export { default as general } from './general.png';
+export { default as footerblackdrop } from './footerblackdrop.jpg';
+export { default as logoblackdrop } from './logoblackdrop.png';
+
+
+export { default as headersocial } from './headersocial.png';
+
+
+
+export { default as vision } from './vision.png';
+export { default as primer_proyecto } from './primer_proyecto.png';
+export { default as hanns } from './hanns.png';
+export { default as mapa } from './mapa.png';
+export { default as climatico } from './climatico.png';
+
+export { default as slider1 } from './slider1.png';
+export { default as slider2 } from './slider2.png';
+export { default as slider3 } from './slider3.png';
