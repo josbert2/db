581912e8dae1a9acb5a3646813a81e44f4416681
unknown <joheandroid@gmail.com>
2023-12-01 10:47:49 -0300

    first commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -40,3 +40,29 @@ export { default as climatico } from './climatico.png';
 export { default as slider1 } from './slider1.png';
 export { default as slider2 } from './slider2.png';
 export { default as slider3 } from './slider3.png';
+
+export { default as compromomiso_ambiental } from './compromiso_ambiental/compromomiso_ambiental.png';
+export { default as integral } from './compromiso_ambiental/integral.png';
+export { default as desafios } from './compromiso_ambiental/desafios.png';
+
+export { default as slidercompromiso1 } from './slidercompromiso1.png';
+export { default as grano1 } from './compromiso_ambiental/slidercompromiso2.png';
+export { default as slidercompromiso2 } from './compromiso_ambiental/slidercompromiso2.png';
+export { default as slidercompromiso3 } from './compromiso_ambiental/slidercompromiso3.png';
+export { default as slidercompromiso4 } from './compromiso_ambiental/slidercompromiso4.png';
+
+export { default as homecompost } from './compromiso_ambiental/homecompost.jpg';
+export { default as compost } from './compromiso_ambiental/compost.jpg';
+export { default as compostindustrial } from './compromiso_ambiental/compostindustrial.png';
+export { default as planaccion } from './compromiso_ambiental/planaccion.png';
+export { default as programa } from './compromiso_ambiental/programa.png';
+export { default as parquearauco } from './compromiso_ambiental/parquearauco.jpg';
+export { default as alto_condes } from './compromiso_ambiental/alto_condes.jpg';
+export { default as objetivos } from './compromiso_ambiental/objetivos.png';
+export { default as disfruta } from './compromiso_ambiental/disfruta.png';
+
+export { default as ma1 } from './ma1.png';
+export { default as ma2 } from './ma2.png';
+export { default as ma3 } from './ma3.png';
+export { default as ma4 } from './ma4.png';
+export { default as ma5 } from './ma5.png';
